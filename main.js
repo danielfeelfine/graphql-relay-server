55fa7a9781c4224a4acbec5b821174eb2e666233
Daniel Torres <danielfeelfine@gmail.com>
2017-03-16 10:00:10 -0300

    (add) graphql settings

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import GraphQLHTTP from 'express-graphql'
 const debug = require('debug')('graphql:server')
 
 const GRAPHQL_PORT = 8080
 
 const server = express()
+server.use('/', GraphQLHTTP({
+  pretty: true,
+  graphiql: true
+}))
 
 server.listen(GRAPHQL_PORT, () => debug(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
